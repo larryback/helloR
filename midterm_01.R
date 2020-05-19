7c68d51e09be1e103f156352a7d6025cfcbf43e7
larryback <41230266+larryback@users.noreply.github.com>
2020-05-20 00:10:35 +0900

    2020년 5월 20일

--- midterm_01.R
+++ midterm_01.R
@@ -107,3 +107,91 @@ nlist
 nlist[[2]][5]
 
 nlist$vec2[c(2, 3)]
+
+d2 <- "D:/exam/story.txt"
+
+d2 <- matrix( c('Lee', 55, 'M', 'Park', 47, 'F', 'So', 37, 'M', 'Kim', 26, 'F', 'Yoon', 29, 'M'), ncol = 3, byrow = T, dimnames = , c("name", "age", "sex"))
+
+
+char1 <- rep(LETTERS[1:3], c(2, 2, 1))
+
+char1
+
+num1 <- rep(1:3, c(2,2,1))
+
+num1
+
+test1 <- data.frame(char1, num1)
+
+test1
+
+name <- c("Lee", "Park", "So","Kim", "Yoon")
+
+name
+
+age <- c(55, 47, 35, 26, 29)
+
+sex <- c("M", "F", "M", "F", "M")
+
+num <- c(1,2,3,4,5)
+
+d2 <- data.frame(num, name, age, sex)
+
+d2
+
+data("state")
+
+data
+
+state.x77[4, 7]
+
+state.x77[c(7,17,28), c(2,5,7)]
+
+state.x77[-c(5:50),c(3:5)]
+
+state.x77["Population">4000,c("Income")]
+
+x <- c(23,34,35,46,55,13,26,34,35,75)
+
+sum <- 0
+
+for (i in x){
+  sum <- sum + i
+ } 
+print(sum/length(x))  
+
+
+mean(x)
+
+k = 7
+
+for (i in c(5, 3, 1, 1, 3, 5)) {
+  
+  k = k - 1
+  print( rep(k, i) )
+  
+} 
+
+x <- c(6,5,4,3,2,1)
+y <- c(5,3,1,1,3,5)
+i = 0 
+
+while(i<6){
+  i = i + 1
+  z <- c(rep(x[i], y[i]))
+  print(z)
+}
+
+
+x <- c(6,5,4,3,2,1)
+y <- c(5,3,1,1,3,5)
+i = 0 
+
+repeat{i = i + 1
+       z <- c(rep(x[i], y[i]))
+       print(z)
+       if(i > 5){
+         break
+       }
+       
+       }
